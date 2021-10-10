a3a43d0d492c23ee758b81040094cd77699ae10d
ElusiveSkies <83562370+ElusiveSkies@users.noreply.github.com>
2021-10-10 16:09:56 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker-oct2021',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+  app.use(express.static("public"));
+
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
